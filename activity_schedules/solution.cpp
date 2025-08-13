43fc215522204d03ed3aa3a209062b10d39553e1
Hashem Erainat <109301790+Hashem-Soud1@users.noreply.github.com>
2025-08-13 22:49:39 +0300

    write solution

--- activity_schedules/solution.cpp
+++ activity_schedules/solution.cpp
@@ -1,1 +1,37 @@
-sdv
+#include <bits/stdc++.h>
+using namespace std;
+
+
+    vector<string> act = {"swimming", "running", "football"};
+    vector<string> sch;
+
+void FindSchedules(int n) {
+    if (sch.size() == n) {
+        for (auto &day : sch) {
+            cout << day << " ";
+        }
+        cout << endl;
+        return;
+    }
+
+    for (auto &activity : act) {
+        if (sch.empty() || sch.back() != activity) {
+
+            sch.push_back(activity);
+            
+            FindSchedules(n);
+            
+            sch.pop_back();
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of days : ";
+    cin >> n;
+
+    FindSchedules(n);
+
+    return 0;
+}
